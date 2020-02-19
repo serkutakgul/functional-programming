c3b8f9b636877186f0dd308f8cfd9a4e0560ab02
serkutakgul <56157543+serkutakgul@users.noreply.github.com>
2020-02-19 17:08:21 +0300

    Predicate Functional Interface Demo

---
+++ src/com/predicate/PredicateDemo.java
@@ -0,0 +1,19 @@
+package com.predicate;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class PredicateDemo{
+
+    //single abstract method:boolean test(T t)
+
+    public static void main(String[] args) {
+
+        Predicate<Integer> predicate= (t)-> t>100;
+        System.out.println(predicate.test(1000));
+
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9);
+        numbers.stream().filter(t -> t%3==0).forEach(t -> System.out.println(t));
+    }
+}
